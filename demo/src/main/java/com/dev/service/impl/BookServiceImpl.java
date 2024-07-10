0da9a67ceda7df74aa97d9a29e21eb2103834486
Zexu Yan <108252463+zexuyann@users.noreply.github.com>
2024-07-10 15:52:43 +0800

    Add files via upload
    init

---
+++ demo/src/main/java/com/dev/service/impl/BookServiceImpl.java
@@ -0,0 +1,40 @@
+package com.dev.service.impl;
+
+import com.dev.controller.Code;
+import com.dev.dao.BookDao;
+import com.dev.domain.Book;
+import com.dev.exception.BusinessException;
+import com.dev.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+    @Autowired
+    private BookDao bookDao;
+
+    public boolean save(Book book) {
+        return bookDao.save(book) > 0;
+    }
+
+    public boolean update(Book book) {
+        return bookDao.update(book) > 0;
+    }
+
+    public boolean delete(Integer id) {
+        return bookDao.delete(id) > 0;
+    }
+
+    public Book getById(Integer id) {
+        if(id == 1){
+            throw new BusinessException(Code.BUSINESS_ERR,"请不要使用你的技术挑战我的耐性!");
+        }
+        return bookDao.getById(id);
+    }
+
+    public List<Book> getAll() {
+        return bookDao.getAll();
+    }
+}
---
+++ demo/src/test/java/com/dev/BookServiceTest.java
@@ -0,0 +1,29 @@
+package com.dev;
+
+import com.dev.service.BookService;
+import com.dev.domain.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class BookServiceTest {
+
+    @Autowired
+    private BookService bookService;
+
+    @Test
+    public void testGetById(){
+        Book book = bookService.getById(2);
+        System.out.println(book);
+    }
+
+    @Test
+    public void testGetAll(){
+        List<Book> all = bookService.getAll();
+        System.out.println(all);
+    }
+
+}
